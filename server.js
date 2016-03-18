556c11fd723a471e5dcad355dcf133c8ad051cb9
Vijay Chhuttani <chhuttani.vijay@gmail.com>
2016-03-19 05:02:25 +0530

    Added Get /todos rute to fetch all to do items

--- server.js
+++ server.js
@@ -2,8 +2,26 @@ var express = require('express');
 var app = new express();
 var PORT = process.env.PORT || 3000;
 
+var todos = [{
+	id: 1,
+	description: 'Go home early',
+	completed: false
+}, {
+	id: 2,
+	description: 'Eat awesome food',
+	completed: true
+}, {
+	id: 3,
+	description: 'Sleep tight',
+	completed: false
+}];
+
 app.get('/', function(req, res) {
 	res.send("To-DO API Route");
+});
+
+app.get('/todos', function (req, res) {
+	res.json(todos);
 });
 
 app.listen(PORT, function () {
