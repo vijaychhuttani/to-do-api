3dc7a42099c9922618f93f5b5886b388ba9975a5
Vijay Chhuttani <chhuttani.vijay@gmail.com>
2016-04-28 12:13:35 +0530

    added underscore js for refractoring the code.

--- server.js
+++ server.js
@@ -1,59 +1,48 @@
 var express = require('express');
 var app = new express();
 var PORT = process.env.PORT || 3000;
+var _ = require('underscore');
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
 var todos = [];
 var todoNextId = 1;
 
-app.get('/', function(req, res) {
-	res.send("To-DO API Route");
+app.get('/', function (req, res) {
+    res.send("To-DO API Route");
 });
 
 app.get('/todos', function (req, res) {
-	res.json(todos);
+    res.json(todos);
 });
 
 app.post('/todos', function (req, res) {
-	var body = req.body;
-	console.log(body);
-	var todo = {};
-	if(typeof body.description !== 'undefined' && body.description !== null){
-		todo.description = body.description;
-		if(typeof body.completed !== 'undefined' && body.completed !== null){
-			todo.completed = body.completed
-		} else {
-			todo.completed = false;
-		}
-		todo.id = todoNextId;
-		todos.push(todo);
-		todoNextId++;
-		res.json(todo);
-	} else {
-		res.send("Error - Please enter description");
-	}
+    var todo = _.pick(req.body, "description", "completed");
+
+    if (!_.isBoolean(todo.completed) || !_.isString(todo.description) || todo.description.trim().length === 0) {
+        res.status(400).send("Bad data provided");
+    } else {
+        todo.description = todo.description.trim();
+        todo.id = todoNextId;
+        todoNextId = todoNextId + 1;
+        console.log(todo);
+        todos.push(todo);
+        res.json(todo);
+    }
 });
 
 app.get('/todos/:id', function (req, res) {
-	var todoId = req.params.id;
-	var todoFound;
-
-	todos.forEach (function(todo){
-		if (todo.id == todoId) {
-			todoFound = todo;
-		}
-	});
-
-	if(!todoFound || todoFound === undefined) {
-		res.status(404).send();
-	} else {
-		res.json(todoFound);
-	}
+    var todoId = parseInt(req.params.id);
+    var todoFound = _.findWhere(todos, {id: todoId});
+    if (!todoFound || todoFound === undefined) {
+        res.status(404).send("No to-do item found with id " + todoId);
+    } else {
+        res.json(todoFound);
+    }
 
 });
 
 app.listen(PORT, function () {
-	console.log("Express server is now listening on port " + PORT + "...");
-})
+    console.log("Express server is now listening on port " + PORT + "...");
+});
 
