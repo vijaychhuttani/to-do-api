5ddf94863ff33f381e64d3b9c57ec5b030abea8b
Vijay Chhuttani <chhuttani.vijay@gmail.com>
2016-05-17 13:30:53 +0530

    mend
    Changed GET todos to fetch data from db

--- server.js
+++ server.js
@@ -17,36 +17,71 @@ app.get('/', function(req, res) {
 //
 app.get('/todos', function(req, res) {
     var queryParams = req.query;
-    var filteredTodos = todos;
+    var searchCriteria = {};
+    var filteredTodos = [];
 
     if (queryParams.hasOwnProperty("completed") && queryParams.completed === 'true') {
-        filteredTodos = _.where(filteredTodos, {
-            "completed": true
-        });
+       searchCriteria.completed = true;
     } else if (queryParams.hasOwnProperty("completed") && queryParams.completed === 'false') {
-        filteredTodos = _.where(filteredTodos, {
-            "completed": false
-        });
+        searchCriteria.completed = false;
     }
 
     if (queryParams.hasOwnProperty("desc") && queryParams.desc.length > 0) {
-        filteredTodos = _.filter(filteredTodos, function(todo) {
-            if (todo.description.indexOf(queryParams.desc) > -1) {
-                return true;
-            } else {
-                return false;
-            }
-        });
+        searchCriteria.description = {
+            $like : '%'+queryParams.desc+'%'
+        }
     }
 
-    if (filteredTodos.length === 0) {
-        return res.status(404).send({
-            "Error": "No such To-Do items found"
-        });
-    }
-    res.json(filteredTodos);
+    console.log(searchCriteria);
+
+    db.todo.findAll({
+        where: searchCriteria
+    }).then(function(todos) {
+        if(todos.length > 0) {
+            todos.forEach(function(todo){
+                //console.log(todo);
+                filteredTodos.push(todo.toJSON());
+            });
+            return res.json(filteredTodos);
+        } else {
+            return res.status(404).send();
+        }
+    }).catch(function(e) {
+        console.error(e);
+        return res.status(500).send(e);
+    })
+
+    // var filteredTodos = todos;
+
+    // if (queryParams.hasOwnProperty("completed") && queryParams.completed === 'true') {
+    //     filteredTodos = _.where(filteredTodos, {
+    //         "completed": true
+    //     });
+    // } else if (queryParams.hasOwnProperty("completed") && queryParams.completed === 'false') {
+    //     filteredTodos = _.where(filteredTodos, {
+    //         "completed": false
+    //     });
+    // }
+
+    // if (queryParams.hasOwnProperty("desc") && queryParams.desc.length > 0) {
+    //     filteredTodos = _.filter(filteredTodos, function(todo) {
+    //         if (todo.description.indexOf(queryParams.desc) > -1) {
+    //             return true;
+    //         } else {
+    //             return false;
+    //         }
+    //     });
+    // }
+
+    // if (filteredTodos.length === 0) {
+    //     return res.status(404).send({
+    //         "Error": "No such To-Do items found"
+    //     });
+    // }
+    // res.json(filteredTodos);
 });
 
+//POST new todo items
 app.post('/todos', function(req, res) {
     var todo = _.pick(req.body, "description", "completed");
 
@@ -73,17 +108,29 @@ app.post('/todos', function(req, res) {
     // return res.json(todo);
 });
 
+//GET todo items by id
 app.get('/todos/:id', function(req, res) {
-    var todoId = parseInt(req.params.id);
-    var todoFound = _.findWhere(todos, {
-        id: todoId
+    var todoId = parseInt(req.params.id, 10);
+
+    db.todo.findById(todoId).then(function(todo) {
+        if (!!todo) {
+            res.json(todo.toJSON());
+        } else {
+            return res.status(404).send();
+        }
+    }).catch(function(e) {
+        res.status(500).send(e);
     });
-    if (!todoFound) {
-        return res.status(404).json({
-            error: "No to-do item found with id " + todoId
-        });
-    }
-    return res.json(todoFound);
+
+    // var todoFound = _.findWhere(todos, {
+    //     id: todoId
+    // });
+    // if (!todoFound) {
+    //     return res.status(404).json({
+    //         error: "No to-do item found with id " + todoId
+    //     });
+    // }
+    // return res.json(todoFound);
 
 });
 
